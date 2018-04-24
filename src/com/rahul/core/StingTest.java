81926f4afc63780fc762296c8e147eacec06b4af
Unify Technologies <Unify@LTA1IHKTG4-MAC.local>
2018-04-24 22:26:48 +0530

    first commit

---
+++ src/com/rahul/core/StingTest.java
@@ -0,0 +1,26 @@
+package com.rahul.core;
+
+public class StingTest {
+
+	public static void main(String[] args) {
+//		String a = new String("kjkjkjkkjkjkjkjkjkjkjruytkjkghfd");
+//		String b = "ABC";
+//		if(a.intern() == b) {
+//			System.out.println("equal");
+//		} else {
+//			System.out.println("not equal");
+//		}
+//		System.out.println(a.intern());
+		
+		int a = 10;
+		int b= 10;
+		System.out.println("Total = "+a+b+" this");
+		String xyz = "10";
+		System.out.println("Str"+getnum());
+		
+	}
+	static public int getnum() {
+		return 7;
+	}
+
+}
